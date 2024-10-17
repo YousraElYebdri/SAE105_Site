dce9efc7c79caca4d00674a65726af7e01f37569
yousra EL YEBDRI <ely.yousra090705@gmail.com>
2024-10-17 09:00:48 +0200

    first commit

---
+++ synthese.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Récapitulatif</h1>
+    <p>Civilité : <?php echo $civilite ?></p>
+    <p>Nom : <?php echo $nom ?></p>
+    <p>Prénom : <?php echo $prenom ?></p>
+    <p>Adresse : <?php echo $adresse ?></p>
+    <p>Date de Naissance : <?php echo $datenaissance ?></p>
+    <p>Adresse mail : <?php echo $mail ?></p>
+    <p>Sport pratiqué  : <?php echo $sport ?></p>
+</body>
+</html>
