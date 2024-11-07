51ebc97ab094665fed28a1f859757a3337c63ad6
yousra EL YEBDRI <ely.yousra090705@gmail.com>
2024-11-07 22:50:55 +0100

    update

--- scripts/form.php
+++ scripts/form.php
@@ -1,12 +1,12 @@
 <?php
 
-    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_SPECIAL_CHARS);
-    $mail = filter_input(INPUT_POST, 'mail', FILTER_SANITIZE_SPECIAL_CHARS);
-    $sport = filter_input(INPUT_POST, 'sport', FILTER_SANITIZE_SPECIAL_CHARS);
-    $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_SPECIAL_CHARS);
-    $adresse = filter_input(INPUT_POST, 'adresse', FILTER_SANITIZE_SPECIAL_CHARS);
-    $civilite = filter_input(INPUT_POST, 'civilite', FILTER_SANITIZE_SPECIAL_CHARS); 
-    $datenaissance = filter_input(INPUT_POST, 'datenaissance', FILTER_SANITIZE_SPECIAL_CHARS);
+    $nom = $_POST['nom'];
+    $mail = $_POST['mail'];
+    $sport = $_POST['sport'];
+    $prenom = $_POST['prenom'];
+    $adresse = $_POST['adresse'];
+    $civilite = $_POST['civilite'];
+    $datenaissance = $_POST['datenaissance'];
 
 
     include('../synthese.php')
